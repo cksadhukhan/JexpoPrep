63399688088692e3d8ef6e4c04cea3942db88e19
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2021-09-01 23:01:21 +0530

    index modified

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,16 @@
-// NOTE: Components will be export here
+export * from './box/box.component'
+export * from './box/box.component.props'
+
+export * from './heading/heading.component'
+export * from './heading/heading.component.props'
+
+export * from './icon/icon.component'
+export * from './icon/icon.props'
+
+export * from './layout'
+
+export * from './text/text.component'
+export * from './text/text.props'
+
+export * from './track/track.component'
+export * from './track/track.component.props'
