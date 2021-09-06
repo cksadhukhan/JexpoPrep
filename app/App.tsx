ac694c17aa36b389dbbfb3049d35d62e7e32193e
Chanchal Sadhukhan <55362491+cksadhukhan@users.noreply.github.com>
2021-09-06 14:57:45 +0530

    Splash screen (#1)
    * packages updated
    
    * loading animation file added
    
    * splash screen added

--- app/App.tsx
+++ app/App.tsx
@@ -6,10 +6,16 @@ import RootNavigator from './navigation/navigation'
 import lightTheme from './theme/light'
 import darkTheme from './theme/dark'
 import {DarkModeContext} from './hooks/useDarkMode'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
   const colorScheme = useColorScheme()
   const [isDarkMode, setIsDarkMode] = useState(false)
+
+  React.useEffect(() => {
+    SplashScreen.hide()
+    // getToken();
+  })
 
   return (
     <DarkModeContext.Provider value={{isDarkMode, setIsDarkMode}}>
